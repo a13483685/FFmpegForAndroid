3e2f29d128c924bfacd2ee68e9dd736c4e55166d
xiezheng <xiez@suchness.cn>
2018-04-02 11:17:32 +0800

    first commit

---
+++ app/src/main/cpp/IDemux.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xiezheng on 2018/4/2.
+//
+
+#include "IDemux.h"
---
+++ app/src/main/cpp/IDemux.h
@@ -0,0 +1,14 @@
+//
+// Created by xiezheng on 2018/4/2.
+//
+
+#ifndef JFZ_IDEMUX_H
+#define JFZ_IDEMUX_H
+
+
+class IDemux {
+
+};
+
+
+#endif //JFZ_IDEMUX_H
