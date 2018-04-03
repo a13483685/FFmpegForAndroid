5aaa15707ae87b1f0dbefa90b4139bc88afcb0b2
xiezheng <xiez@suchness.cn>
2018-04-03 17:29:19 +0800

    FFDemux register ffmpeg

--- app/src/main/cpp/FFDemux.cpp
+++ app/src/main/cpp/FFDemux.cpp
@@ -4,6 +4,11 @@
 
 #include "FFDemux.h"
 #include "Xlog.h"
+#include <libavformat/avformat.h>
+
+extern "C" {
+//#include <>
+}
 
 bool FFDemux::open(const char* url)
 {
@@ -15,4 +20,23 @@ XDATA FFDemux::read()
 {
     XDATA d;
     return d;
+}
+
+FFDemux::FFDemux()
+{
+    static bool isFirst = true;
+    if (isFirst)
+    {
+        isFirst = false;
+        av_register_all();
+
+        //注册所有的解码器 不用判断是否多次注册 ffmpeg内部有机制判断是否之前有注册
+        avcodec_register_all();
+
+        avformat_network_init();
+
+        XLOGI("register ffmpeg");
+
+    }
+
 }
--- app/src/main/cpp/FFDemux.h
+++ app/src/main/cpp/FFDemux.h
@@ -10,11 +10,15 @@
 #include "IDemux.h"
 
 class FFDemux:public IDemux {
+
     //virtual不删除 没有啥意义但是可以提醒调用者，这部分是用于重载的
     //打开文件
     virtual bool open(const char* url) ;
     //读取一帧数据，由调用者清理，以免发生内存的泄露
     virtual XDATA read() ;
+
+public:
+    FFDemux();
 };
 
 
